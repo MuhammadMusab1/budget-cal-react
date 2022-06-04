34bf7f295c584a831cca8d0321e0463fe691a963
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-06-03 20:29:43 -0500

    add a useState of allItems and render them dynamically

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./style.css";
+import { useState, useEffect } from "react";
 //Components Import
 import { BudgetSummary } from "./components/BudgetSummary";
 import { IncomeList } from "./components/IncomeList";
@@ -7,6 +8,28 @@ import { ExpenseList } from "./components/ExpenseList";
 import { Add } from "./components/Add";
 
 function App() {
+  const InitialState = [
+    {
+      id: 1,
+      description: "Car Payment",
+      value: 200,
+      date: new Date().toDateString(),
+    },
+    {
+      id: 2,
+      description: "Pocket Money",
+      value: 20,
+      date: new Date().toDateString(),
+    },
+    {
+      id: 3,
+      description: "Groceries",
+      value: -100,
+      date: new Date().toDateString(),
+    },
+  ];
+  const [allItems, setAllItems] = useState(InitialState);
+
   return (
     <div className="app">
       <div className="top">
@@ -15,8 +38,8 @@ function App() {
       <div className="bottom">
         <Add />
         <div className="container">
-          <IncomeList />
-          <ExpenseList />
+          <IncomeList list={allItems} />
+          <ExpenseList list={allItems} />
         </div>
       </div>
     </div>
